ab9b3b05d6fa9eec98872e5791ba0f0dd65896d3
Forlorn4eva <darko123Z>
2022-07-23 18:06:21 +0545

    FIN

--- product_module/views.py
+++ product_module/views.py
@@ -21,24 +21,27 @@ def index(request):
             products = Product.objects.filter(filter_query)
         else:
             products = Product.objects.all()
-            categories = Category.objects.all()
-            brands = Brand.objects.all()
-            context = {
-                'products': products,
-                'categories': categories,
-                'brands': brands,
-                'search_query': '',
-            }
-            return render(request, 'index.html', context)
+
+        
+        categories = Category.objects.all()
+
+        brands = Brand.objects.all()
+        context = {
+            'products': products,
+            'categories': categories,
+            'brands': brands,
+            'search_query': '',
+        }
+
+        return render(request, 'index.html', context)
+
     elif request.method == "POST":
         q = request.POST.get("query")
         if "-" in q:
             price_values = q.split("-")
-            filter_query = Q(price__gte=price_values[0]) & Q(
-                price__lte=price_values[1])
+            filter_query = Q(price__gte=price_values[0]) & Q(price__lte=price_values[1])
         else:
-            filter_query = Q(name__icontains=q) | Q(
-                price__icontains=q) | Q(brand__name__icontains=q)
+            filter_query = Q(name__icontains=q) | Q(price__icontains=q) | Q(brand__name__icontains=q)
         products = Product.objects.filter(filter_query)
         categories = Category.objects.all()
         brands = Brand.objects.all()
@@ -48,70 +51,73 @@ def index(request):
             'brands': brands,
             'search_query': q,
         }
+
         return render(request, 'index.html', context)
 
-        # TODO for CartItem login
 
 
-@login_required(login_url="/admin/login")
+#TODO for CartItem login
+
+
+@login_required(login_url = "/admin/login")
+
 def cart(request):
-    # get request data
+    #get request data
     product_id = request.GET.get("id")
     quantity = request.GET.get("qty")
-
+    
     if product_id:
-        # retrieve product data
+        #retrieve product data
         product = Product.objects.get(id=product_id)
-
+        
         try:
             # get cart item and increase quantity
-            cart_item = CartItem.objects.get(
-                user=request.user, product=product)
+            cart_item = CartItem.objects.get(user=request.user, product=product)
             cart_item.quantity += int(quantity)
             cart_item.entered_on = datetime.now()
-
+            
         except CartItem.DoesNotExist:
             # initialize cart item
             cart_item = CartItem(
-                user=request.user,
+                user = request.user,
                 product=product,
                 quantity=int(quantity),
-                entered_on=datetime.now(),
+                entered_on = datetime.now(),
             )
         # Save to database
-
+        
         cart_item.save()
-
-    # retrive the cart items for the user from db
-
+        
+    #retrive the cart items for the user from db
+    
     cart_items = CartItem.objects.filter(user=request.user)
-
-    # calculate total
+    
+    # calculate total 
     total = 0
     for item in cart_items:
         total += item.product.price * item.quantity
-
+        
     # return view
-
+    
     context = {
         'cart_items': cart_items,
         'total': total,
-    }
-
+    }     
+    
     return render(request, "cart.html", context)
-
 
 def removecart(request, id):
     try:
         # get cart item and remove it
         product = Product.objects.get(id=id)
-        cart_item = CartItem.objects.get(user=request.user, product=product)
+        cart_item = CartItem.objects.get(user = request.user, product=product)
         cart_item.delete()
-
+    
     except CartItem.DoesNotExist:
         pass
-
-    # redirect to cart
+    
+    
+    # redirect to cart 
     return redirect(cart)
 
 
